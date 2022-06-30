6946037520559ddbe684cf8377ec141dad2ae9b6
KyuMin Jeong <qmin80@gmail.com>
2022-06-30 13:07:05 +0900

    Add daemon check

--- indep_node_alarm_check.py
+++ indep_node_alarm_check.py
@@ -27,4 +27,18 @@ while True:
         except:
             pass
 
+    daemon_name = "daemonname"
+    daemon_status = subprocess.check_output(eval(f'f"""service {daemon_name} status | grep Active"""'), shell=True).decode('utf-8')
+    daemon_status = str(daemon_status).split(":")[1].strip()[:6]
+
+    if daemon_status != "active":
+        alarm_content = daemon_name + " is NOT active, check this node : " + node_name
+ 
+        try:
+            requestURL = "https://api.telegram.org/bot" + str(telegram_token) + "/sendMessage?chat_id=" + telegram_chat_id + "&text="
+            requestURL = requestURL + str(alarm_content)
+            response = requests.get(requestURL, timeout=1)
+        except:
+            pass
+
     time.sleep(check_internal)
